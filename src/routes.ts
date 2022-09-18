64abfcc38fc6aa2394896c49bbca6c4b3f24dc08
junioralcant <junioralcant111@gmail.com>
2022-09-18 17:45:24 -0300

    Create deliveryman registration

--- src/routes.ts
+++ src/routes.ts
@@ -1,14 +1,17 @@
 import { Router } from 'express';
 import { AutheticateClientController } from './modules/account/authenticateClient/AutheticateClientController';
 import { CreateClientController } from './modules/clients/useCases/createClient/CreateClientController';
+import { CreateDeliverymanController } from './modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController';
 
 const routes = Router();
 
 const createClienteController = new CreateClientController();
 const authenticateClientController =
   new AutheticateClientController();
+const createDeliverymanController = new CreateDeliverymanController();
 
 routes.post('/client', createClienteController.handler);
 routes.post('/authenticate', authenticateClientController.handler);
+routes.post('/deliveryman', createDeliverymanController.handler);
 
 export { routes };
