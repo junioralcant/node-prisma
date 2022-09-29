c77f4cc445e35a5c7235c16264a2e1ee95af1401
junioralcant <junioralcant111@gmail.com>
2022-09-29 16:04:20 -0300

    Config test

---
+++ src/modules/clients/useCases/createClient/CreateClienteUseCase.spec.ts
@@ -0,0 +1,8 @@
+describe('Create Client', () => {
+  it('Should be able to create a new client', () => {
+    const soma = 2 + 2;
+    const resulte = 3;
+
+    expect(soma).toBe(resulte);
+  });
+});
